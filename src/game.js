6fa0023e68b79f5d8b343dd195a1e127d406b6eb
David Barth <76754802+barthdavid@users.noreply.github.com>
2021-12-30 20:45:50 +0100

    add game reset

--- src/game.js
+++ src/game.js
@@ -10,7 +10,7 @@ import {
 } from "./snake.js";
 import { update as updateFood, draw as drawFood } from "./food.js";
 import { borderCollision } from "./grid.js";
-import { getInputdirection } from "./input.js";
+import { getInputdirection, setInputDirection } from "./input.js";
 
 const GAME_BOARD = document.getElementById("gameBoard");
 const INITIAL_LENGTH = 15;
@@ -18,9 +18,16 @@ let lastRenderTime = 0;
 let gameOver = false;
 let cycleCount = 0;
 
+checkStart();
+
 function main(currentTime) {
     if (gameOver) {
-
+        gameOver = false;
+        GAME_BOARD.innerHTML = '';
+        setInputDirection({ x: 0, y: 0 });
+        resetSnake();
+        draw(cycleCount);
+        checkStart();
         return;
     }
     window.requestAnimationFrame(main);
@@ -52,10 +59,12 @@ function checkCollision() {
     gameOver = borderCollision(getSnakeHead()) || snakeCollision();
 }
 
-window.addEventListener("keydown", (e) => {
-    if (e) {
+function checkStart() {
+    if (getInputdirection().x === 0 && getInputdirection().y === 0) {
+        window.setTimeout(checkStart, 100);
+    } else {
+        expandSnake(7);
         window.requestAnimationFrame(main);
     }
-});
+}
 
-expandSnake(7);
