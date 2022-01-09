9dd1bb70f31825b2d742dfef61fc96e051e6e5ed
David Barth <76754802+barthdavid@users.noreply.github.com>
2022-01-09 18:28:54 +0100

    refactor: remove second break statement

--- src/js/input.js
+++ src/js/input.js
@@ -1,23 +1,27 @@
 let inputDirection = { x: 0, y: 0 };
 let lastInputDirection;
 
-window.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", (e) => {
     switch (e.key) {
         case "ArrowLeft":
-            if (lastInputDirection.x !== 0) break;
-            inputDirection = { x: -1, y: 0 };
+            if (lastInputDirection.x === 0) {
+                inputDirection = { x: -1, y: 0 };
+            }
             break;
         case "ArrowUp":
-            if (lastInputDirection.y !== 0) break;
-            inputDirection = { x: 0, y: -1 };
+            if (lastInputDirection.y === 0) {
+                inputDirection = { x: 0, y: -1 };
+            }
             break;
         case "ArrowRight":
-            if (lastInputDirection.x !== 0) break;
-            inputDirection = { x: 1, y: 0 };
+            if (lastInputDirection.x === 0) {
+                inputDirection = { x: 1, y: 0 };
+            }
             break;
         case "ArrowDown":
-            if (lastInputDirection.y !== 0) break;
-            inputDirection = { x: 0, y: 1 };
+            if (lastInputDirection.y === 0) {
+                inputDirection = { x: 0, y: 1 };
+            }
             break;
     }
 });
