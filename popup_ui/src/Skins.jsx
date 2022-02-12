5de0d514aa1450c49453401d42ed5f63f540d7c0
David Barth <76754802+barthdavid@users.noreply.github.com>
2022-02-12 23:39:27 +0100

    refac: loop over skins

--- popup_ui/src/Skins.jsx
+++ popup_ui/src/Skins.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
 import './skins.css';
 
 function Skins() {
-  function setSkin(color) {
+  const [skins, setSkins] = useState([
+    { id: 1, color: '#ffffff', active: false, name: 'White' },
+    { id: 2, color: '#000000', active: false, name: 'Black' }
+  ]);
+  function updateSkin(color) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { color: color });
     });
   }
   return (
     <ul className="skinList">
-      <li className="skinWrapper">
-        <div className="skinContent" onClick={() => setSkin('#ffffff')}>
-          White
-        </div>
-      </li>
-      <li className="skinWrapper">
-        <div className="skinContent" onClick={() => setSkin('#000000')}>
-          Black
-        </div>
-      </li>
+      {skins.map((skin) => (
+        <li className="skinWrapper">
+          <div
+            key={skin.id}
+            className="skinContent"
+            onClick={() => updateSkin(skin.color)}
+          >
+            {skin.name}
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
