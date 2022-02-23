68e54a12a4272dcc38840d5b1cfdcb282d85636f
David Barth <76754802+barthdavid@users.noreply.github.com>
2022-02-23 23:04:57 +0100

    style: add selected skin indicator

--- popup_ui/src/Skins.jsx
+++ popup_ui/src/Skins.jsx
@@ -3,24 +3,28 @@ import { useState } from 'react';
 import './skins.css';
 
 function Skins() {
-  const [skins, setSkins] = useState([
-    { id: 1, color: '#ffffff', active: false, name: 'White' },
-    { id: 2, color: '#000000', active: false, name: 'Black' }
-  ]);
-  function updateSkin(color) {
+  const [activeSkin, setActiveSkin] = useState();
+  const skins = [
+    { id: 0, color: '#ffffff', name: 'White' },
+    { id: 1, color: '#000000', name: 'Black' }
+  ];
+
+  function updateSkin(skin) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { color: color });
+      chrome.tabs.sendMessage(tabs[0].id, { color: skin.color });
     });
+    setActiveSkin(skin.id);
   }
   return (
     <ul className="skinList">
       {skins.map((skin) => (
-        <li className="skinWrapper">
-          <div
-            key={skin.id}
-            className="skinContent"
-            onClick={() => updateSkin(skin.color)}
-          >
+        <li
+          key={skin.id}
+          className={
+            'skinWrapper' + (skin.id == activeSkin ? ' activeSkinWrapper' : '')
+          }
+        >
+          <div className="skinContent" onClick={() => updateSkin(skin)}>
             {skin.name}
           </div>
         </li>
