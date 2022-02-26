bef76b1fd50c13329600de71139afed1bc0c4b74
David Barth <76754802+barthdavid@users.noreply.github.com>
2022-02-26 22:29:10 +0100

    fix: remember active skin indicator after reopening popup

--- popup_ui/src/Skins.jsx
+++ popup_ui/src/Skins.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import './skins.css';
 
 function Skins() {
-  const [activeSkin, setActiveSkin] = useState();
+  const [activeSkin, setActiveSkin] = useState(
+    chrome.storage.local.get('score').score
+  );
   const skins = [
     { id: 0, color: '#ffffff', name: 'White' },
     { id: 1, color: '#000000', name: 'Black' }
   ];
+
+  useEffect(() => {
+    chrome.storage.local.get('skin').then((skin) => setActiveSkin(skin.skin));
+  });
 
   function updateSkin(skin) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { color: skin.color });
     });
     setActiveSkin(skin.id);
+    chrome.storage.local.set({ skin: skin.id });
   }
   return (
     <ul className="skinList">
